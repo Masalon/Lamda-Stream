7e613f915cad51b57beaa7fbd6ae91f66bd8e980
Masa <107531092+Masalon@users.noreply.github.com>
2022-07-28 14:42:48 +0900

    課題提出

--- src/Chapter10.java
+++ src/Chapter10.java
@@ -1,7 +1,7 @@
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Chapter10 {
 	public static void main(String[] args) throws Exception {
@@ -12,21 +12,26 @@ public class Chapter10 {
 		list.add(new Task(LocalDate.of(2021, 8, 10), "散髪に行く", false));
 		list.add(new Task(LocalDate.of(2021, 11, 9), "スクールの課題を解く", false));
 
-		// make a list consisting of only the incomplete tasks
-		List<Task> incompleteTasks = new ArrayList<>();
-		for (Task var : list) {
-			if (var.isDone() == false) {
-				incompleteTasks.add(var);
-			}
-		}
-		// make it print the number of incomplete tasks
+		// filter through the "false" stuff and add it to the incompleteTasks list
+		List<Task> incompleteTasks = list.stream().filter(var -> var.isDone() == false).collect(Collectors.toList());
+		// print out the size of the list
 		System.out.println("未完了のタスクの個数は" + incompleteTasks.size());
-		// make it sort the 3 incomplete tasks
-		Collections.sort(incompleteTasks);
+		// print out the line told to print
 		System.out.println("【未完了のタスクを昇順に並び替えて一覧表示】");
-		// make it print each task individually
-		for (Task aTask : incompleteTasks) {
-			System.out.println(aTask);
-		}
+		// sort the list in order and print each one
+		incompleteTasks.stream().sorted().forEach(System.out::println);
 	}
 }
+
+// List<Task> incompleteTasks = new ArrayList<>();
+// for (Task var : list) {
+// if (var.isDone() == false) {
+// incompleteTasks.add(var);
+
+// System.out.println("未完了のタスクの個数は" + incompleteTasks.size());
+// make it sort the 3 incomplete tasks
+// Collections.sort(incompleteTasks);
+// System.out.println("【未完了のタスクを昇順に並び替えて一覧表示】");
+// make it print each task individually
+// for (Task aTask : incompleteTasks) {
+// System.out.println(aTask);
